6b85629efd7a9fa1713137912d9af8594aeb7c0c
Louis Vanduyfhuys <Louis.Vanduyfhuys@Ugent.be>
2023-11-08 11:10:10 +0100

    Fixed bug in fep.py import statements

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import numpy
 
 setup(
     name='ThermoLIB',
-    version='1.0.0',
+    version='1.6.0',
     description='Python library with various sub packages related to thermodynamic and free energy profiles.',
     author='Louis Vanduyfhuys',
     author_email='Louis.Vanduyfhuys@UGent.be',
